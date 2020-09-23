745f173e492a9b8e0f4dc1afad77c97eded40d28
tikUI <1518491547@qq.com>
2020-09-23 21:06:06 +0800

    项目合并检车

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -4,12 +4,14 @@ import { withRouter } from 'react-router-dom';
 import { ListView } from 'antd-mobile';
 import { NavBar, Icon } from 'antd-mobile';
 import Recommend from './views/Recommend/recommend.js'
+import Header from './views/Header.js'
 class App extends React.Component {
 
   render() {
     return (
       <div className="App">
-
+	  <Header />
+		<Recommend />
       </div>
     );
   }
