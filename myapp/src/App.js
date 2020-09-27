7760c0d3f5be2d698d07d686aecd54f4150991c7
黎立坤 <lilikun@lilikundeMacBook-Pro.local>
2020-09-27 16:35:41 +0800

    完善路由配置，实现我的页面跳转完善我的收藏页面

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { withRouter ,Route,Redirect,Switch} from 'react-router-dom';
 import { ListView } from 'antd-mobile';
 import { NavBar, Icon } from 'antd-mobile';
+import Mygame from './views/Mine/Mygame';
 const New = lazy(()=> import('./views/News/New'))  
 const Recommend =lazy(()=> import('./views/Recommend/recommend.js'))  
 const Login = lazy(()=> import('./views/Login/Login'))
@@ -10,7 +11,7 @@ const Reg = lazy(()=> import('./views/Reg/Reg'))
 const Mine = lazy(()=> import('./views/Mine/Mine'))
 const Search =lazy(()=> import("./views/Search/Search"))
 const Rank =lazy(()=> import("./views/Rank/Rank"))
-const Collection =lazy(()=> import("./views/Collection/Collection"))
+const MYgame =lazy(()=> import("./views/Mine/Mygame"))
 class App extends React.Component {
 
   render() {
@@ -25,8 +26,8 @@ class App extends React.Component {
 			<Route path="/search" component={Search} />
 		  <Route path="/recommend" component={Recommend} />
       <Route path="/rank" component={Rank} />
+	  <Route path="/mygame" component={Mygame} />
 		  <Route path="/notfound" render={()=><div>404</div>} />
-      <Route path="/collection" component={Collection} />
 		  <Redirect from="/" to="/recommend" exact/>
 		  </Switch>
 		   </Suspense>
